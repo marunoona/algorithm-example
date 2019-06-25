ee16ace58c40d35fca786327d9df5a2af572b3c8
marunoona <ye_jin92@naver.com>
2019-06-25 16:57:38 +0900

    Modify Selection Sort

--- src/sorting/Selection.java
+++ src/sorting/Selection.java
@@ -13,24 +13,12 @@ public class Selection {
                     min = j;
                 }
             }
-            swap(data, min, i);
+            Util.swap(data, min, i);
             System.out.println("선택 정렬 " + (i + 1) + " 단계: ");
-            printArr(data);
+            Util.printArr(data);
         }
     }
 
-    private void swap(int[] arr, int i, int j){
-        int tmp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = tmp;
-    }
-
-    private void printArr(int[] arr){
-        for (Integer n : arr) {
-            System.out.print(n+" ");
-        }
-        System.out.println();
-    }
     public static void main(String[] args) {
         Selection selection = new Selection();
         int[] data = {69, 10, 30, 2, 16, 8};
@@ -40,6 +28,6 @@ public class Selection {
 
         //정렬된 배열 출력
         System.out.println("--------------------\n선택 정렬 최종 결과:");
-        selection.printArr(data);
+        Util.printArr(data);
     }
 }
