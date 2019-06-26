6d4be8b85245b68fcaf71b06426311a36f61a730
marunoona <ye_jin92@naver.com>
2019-06-26 14:05:58 +0900

    Modify Merge Sort

--- src/sorting/Merge.java
+++ src/sorting/Merge.java
@@ -1,25 +1,38 @@
 package sorting;
 
 public class Merge {
+    /**
+     *
+     * @param data 정렬해야 할 데이터 배열
+     * @param p data 배열의 시작 인덱스
+     * @param q data 배열의 마지막 인덱스
+     */
     private void mergeSort(int[] data, int p, int q) {
-        int mid;
+        int mid;    //중간값 (나눠야하니까)
         if (p < q) {
             mid = (p + q) / 2;
-            mergeSort(data, p, mid);
-            mergeSort(data, mid + 1, q);
-            merge(data, p, mid, q);
+            mergeSort(data, p, mid);        //시작부터 중간까지
+            mergeSort(data, mid + 1, q);    //중간부터 끝까지
+            merge(data, p, mid, q);         //합치기
         }
     }
 
+    /**
+     *
+     * @param data
+     * @param p
+     * @param mid
+     * @param q
+     */
     private void merge(int[] data, int p, int mid, int q) {
         int i, j, k;
         i = p;
         j = mid + 1;
-        k = p;
+        k = p;  //추가 배열의 위치 인덱스
         int size = data.length;
-        int[] tmp = new int[size];
-        while (i <= mid && j <= q) {
-            if (data[i] <= data[j])
+        int[] tmp = new int[size];  //합병하기 위한 추가 배열
+        while (i <= mid && j <= q) {    //분할된 집합 중 한쪽이 데이터가 없을때까지
+            if (data[i] <= data[j])     //정렬된 집합의 값을 비교함
                 tmp[k++] = data[i++];
             else
                 tmp[k++] = data[j++];
